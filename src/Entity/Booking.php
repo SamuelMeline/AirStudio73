f3bba2ddff6fd42a83ae0b994427940ae2d04cd3
Samuel MELINE <melsam0905@gmail.com>
2024-07-22 00:08:03 +0200

    add differents modes de paiements pour les abonnements annuelles + quelques fonctrionnalitées

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -26,6 +26,12 @@ class Booking
     #[ORM\Column(type: 'integer', nullable: true)]
     private ?int $numOccurrences = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $paymentMode = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $promoCode;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +81,30 @@ class Booking
     public function setNumOccurrences(?int $numOccurrences): self
     {
         $this->numOccurrences = $numOccurrences;
+
+        return $this;
+    }
+
+    public function getPaymentMode(): ?string
+    {
+        return $this->paymentMode;
+    }
+
+    public function setPaymentMode(?string $paymentMode): self
+    {
+        $this->paymentMode = $paymentMode;
+
+        return $this;
+    }
+
+    public function getPromoCode(): ?string
+    {
+        return $this->promoCode;
+    }
+
+    public function setPromoCode(?string $promoCode): self
+    {
+        $this->promoCode = $promoCode;
 
         return $this;
     }
