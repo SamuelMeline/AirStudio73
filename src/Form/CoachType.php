98668efadea200e84ee38cedb37347e92e8295be
Samuel MELINE <melsam0905@gmail.com>
2024-08-02 17:45:28 +0200

    ajout des différents crud coachs cours events

---
+++ src/Form/CoachType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Coach;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class CoachType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('biography', TextareaType::class, [
+                'label' => 'Biographie',
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo',
+                'required' => false,
+                'mapped' => false, // Indicate that this field is not associated with any entity property
+                'attr' => ['class' => 'form-control-file']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Coach::class,
+        ]);
+    }
+}
---
+++ src/Form/EventType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('startTime', DateTimeType::class, [
+                'label' => 'Début',
+                'widget' => 'single_text',
+            ])
+            ->add('endTime', DateTimeType::class, [
+                'label' => 'Fin',
+                'widget' => 'single_text',
+            ])
+            ->add('location', TextType::class, [
+                'label' => 'Lieu',
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image de l\'événement (JPEG ou PNG)',
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG ou PNG)',
+                    ])
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
