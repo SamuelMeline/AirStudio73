6794846c15898f0ac8ef4bd80945ed6034d19d5b
Samuel MELINE <melsam0905@gmail.com>
2024-09-02 14:55:53 +0200

    update planning

---
+++ migrations/Version20240902124147.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240902124147 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, course_id INT NOT NULL, user_id INT NOT NULL, subscription_course_id INT NOT NULL, is_recurrent TINYINT(1) NOT NULL, num_occurrences INT NOT NULL, INDEX IDX_E00CEDDE591CC992 (course_id), INDEX IDX_E00CEDDEA76ED395 (user_id), INDEX IDX_E00CEDDE6BA30B2E (subscription_course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE coach (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, biography LONGTEXT DEFAULT NULL, photo VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, capacity INT NOT NULL, is_recurrent TINYINT(1) NOT NULL, recurrence_interval INT DEFAULT NULL, recurrence_duration VARCHAR(255) DEFAULT NULL, is_canceled TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course_details (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, photo VARCHAR(255) DEFAULT NULL, default_capacity INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, location VARCHAR(255) NOT NULL, photo VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, image_path VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE plan (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, stripe_price_id VARCHAR(255) NOT NULL, max_payments VARCHAR(255) DEFAULT NULL, start_date DATE DEFAULT NULL, end_date DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE plan_course (id INT AUTO_INCREMENT NOT NULL, plan_id INT NOT NULL, course_id INT NOT NULL, credits INT NOT NULL, INDEX IDX_A2C677CBE899029B (plan_id), INDEX IDX_A2C677CB591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE promo_code_usage (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, promo_code VARCHAR(255) NOT NULL, used_at DATETIME NOT NULL, INDEX IDX_6025E75FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE schedule (id INT AUTO_INCREMENT NOT NULL, day VARCHAR(255) NOT NULL, start_time TIME NOT NULL, end_time TIME NOT NULL, course_name VARCHAR(255) NOT NULL, instructor VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, plan_id INT NOT NULL, purchase_date DATE NOT NULL, expiry_date DATE DEFAULT NULL, payment_mode VARCHAR(255) DEFAULT NULL, payments_count INT NOT NULL, max_payments INT NOT NULL, is_active TINYINT(1) NOT NULL, stripe_subscription_id VARCHAR(255) DEFAULT NULL, promo_code VARCHAR(255) DEFAULT NULL, INDEX IDX_A3C664D3A76ED395 (user_id), INDEX IDX_A3C664D3E899029B (plan_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE subscription_course (id INT AUTO_INCREMENT NOT NULL, subscription_id INT NOT NULL, course_id INT NOT NULL, remaining_credits INT NOT NULL, INDEX IDX_DF68773E9A1887DC (subscription_id), INDEX IDX_DF68773E591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, address VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, notes LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE6BA30B2E FOREIGN KEY (subscription_course_id) REFERENCES subscription_course (id)');
+        $this->addSql('ALTER TABLE plan_course ADD CONSTRAINT FK_A2C677CBE899029B FOREIGN KEY (plan_id) REFERENCES plan (id)');
+        $this->addSql('ALTER TABLE plan_course ADD CONSTRAINT FK_A2C677CB591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE promo_code_usage ADD CONSTRAINT FK_6025E75FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3E899029B FOREIGN KEY (plan_id) REFERENCES plan (id)');
+        $this->addSql('ALTER TABLE subscription_course ADD CONSTRAINT FK_DF68773E9A1887DC FOREIGN KEY (subscription_id) REFERENCES subscription (id)');
+        $this->addSql('ALTER TABLE subscription_course ADD CONSTRAINT FK_DF68773E591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE591CC992');
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDEA76ED395');
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE6BA30B2E');
+        $this->addSql('ALTER TABLE plan_course DROP FOREIGN KEY FK_A2C677CBE899029B');
+        $this->addSql('ALTER TABLE plan_course DROP FOREIGN KEY FK_A2C677CB591CC992');
+        $this->addSql('ALTER TABLE promo_code_usage DROP FOREIGN KEY FK_6025E75FA76ED395');
+        $this->addSql('ALTER TABLE subscription DROP FOREIGN KEY FK_A3C664D3A76ED395');
+        $this->addSql('ALTER TABLE subscription DROP FOREIGN KEY FK_A3C664D3E899029B');
+        $this->addSql('ALTER TABLE subscription_course DROP FOREIGN KEY FK_DF68773E9A1887DC');
+        $this->addSql('ALTER TABLE subscription_course DROP FOREIGN KEY FK_DF68773E591CC992');
+        $this->addSql('DROP TABLE booking');
+        $this->addSql('DROP TABLE coach');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE course');
+        $this->addSql('DROP TABLE course_details');
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE photo');
+        $this->addSql('DROP TABLE plan');
+        $this->addSql('DROP TABLE plan_course');
+        $this->addSql('DROP TABLE promo_code_usage');
+        $this->addSql('DROP TABLE schedule');
+        $this->addSql('DROP TABLE subscription');
+        $this->addSql('DROP TABLE subscription_course');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
